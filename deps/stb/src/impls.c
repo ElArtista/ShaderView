abae35e2d93df8f1857f676c4a7fd41f536767e8
TheArtist <agorglouk@gmail.com>
2016-03-25 14:02:33 +0200

    Added stb dependency

---
+++ deps/stb/src/impls.c
@@ -0,0 +1,13 @@
+/**
+ * Header including all the stb library implementations
+ * for using them with precompiled modules
+ */
+
+#define STB_TRUETYPE_IMPLEMENTATION
+#include <stb_truetype.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
